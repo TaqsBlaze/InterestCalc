7ccb672571f3aba5bbb0fd63857f5978ef6f85c3
TaqsBlaze <33166177+TaqsBlaze@users.noreply.github.com>
2019-04-20 21:28:01 +0200

    Delete Extras.py

--- Extras.py
+++
@@ -1,8 +0,0 @@
-from Tkinter import(
-    Frame,
-    RIDGE,
-)
-def extras():
-    result_container=Frame();
-    result_container.configure(width=920,height=170,relief=RIDGE,border=4,bg='white');
-    result_container.place(x=15,y=430);
