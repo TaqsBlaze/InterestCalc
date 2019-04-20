92cca7206841c474133f597999cd79d07a847ce5
TaqsBlaze <33166177+TaqsBlaze@users.noreply.github.com>
2019-04-20 12:36:15 +0200

    Add files via upload
    Most of the classes are now in there separate files

---
+++ files/files/Months.py
@@ -0,0 +1,17 @@
+from Tkinter import Label
+from files import SplashScreen
+'''
+p=princepal
+r=rate
+t=time
+VAR= the value to display time/period if months or years was ticked
+'''
+def calcMonths(Container,var,p,r,t):
+        Interest=(float(p)*float(r))*(float(t)/12);
+        Interest = float(Interest/100);
+        Container.configure(text="Interest: $ {:,.2f}".format(Interest));
+        total= float(p) + Interest;
+        Total = Label(Container,text='Total: $ {:,.2f}'.format(total),bg='lightgray',font='FB 12 underline');
+        Total.place(x=45,y=68);
+        VAR = var;
+        SplashScreen._more_(VAR,p,r,t,Interest,total);
