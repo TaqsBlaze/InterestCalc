0be7c04d1d3691b63589d1de9e340f0977160215
Admire Murove <amurove@kreamorn.co.zw>
2019-06-26 13:59:46 +0200

    Update

--- files/files/__init__.py
+++
@@ -1,3 +0,0 @@
-'''
-Dumy file for importing patchies
-'''
--- files/files/settings/OSconfig.py
+++
@@ -1,8 +0,0 @@
-from os import name
-
-def Os(Window):
-    Os_x0= name
-    if(Os_x0 != "nt"):
-        Window.distroy()
-    else:
-        pass
