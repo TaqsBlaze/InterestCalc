a5ed785537e1dd5a6ac09b5c08ebeb1b7add1783
TaqsBlaze <33166177+TaqsBlaze@users.noreply.github.com>
2020-01-06 09:28:37 +0200

    Fixed yearly calculation error
    the time variable was being calculated twice

--- files/Years.py
+++ files/Years.py
@@ -1,9 +1,9 @@
 import SplashScreen
 from Tkinter import Label
-def Calculate(Container,var,p,r,t,screen):
+def Calculate(Container,var,p,r,t,screen,tab1):
        
         try:
-            Interest = (float(p)*float(r)*float(r)*(float(t)))/100;
+            Interest = (float(p)*float(r)*float(t))/100;
             _InitCount_ = Interest;
             Container.configure(text='{} {}'.format('Interest: $','{:,.2f}'.format(_InitCount_)))
             total= float(p) + _InitCount_
@@ -11,7 +11,7 @@ def Calculate(Container,var,p,r,t,screen):
             Total.place(x=45,y=68)
             screen.update();
             VAR = var
-            SplashScreen._more_(VAR,p,r,t,_InitCount_,total)
+            SplashScreen._more_(VAR,p,r,t,_InitCount_,total,tab1)
         except Exception,e:
             print 'Error in Years:',e;
             pass
